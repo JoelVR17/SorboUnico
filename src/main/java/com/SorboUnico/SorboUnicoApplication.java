8ab1c76cf4baa0f8fa19b879206ecdeca68a6fb1
JoelVR17 <vermudo.com@gmail.com>
2022-03-18 14:41:00 -0600

    Primer el Commit

---
+++ src/main/java/com/SorboUnico/SorboUnicoApplication.java
@@ -0,0 +1,13 @@
+package com.SorboUnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorboUnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorboUnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SorboUnico/SorboUnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SorboUnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorboUnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
